ec0c81a1b9336fbae9b3701221308e7527feb17c
Minh Ngoc <minhngoc2512@yahoo.com>
2023-07-14 15:47:05 +0700

    update

--- src/middleware/FilterRequestForApi.php
+++ src/middleware/FilterRequestForApi.php
@@ -1,8 +1,23 @@
 <?php
 
 namespace Ngocnm\LaravelHelpers\middleware;
+use Closure;
+use Illuminate\Http\Request;
+use Ngocnm\LaravelHelpers\Helper;
 
 class FilterRequestForApi
 {
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure(\Illuminate\Http\Request): (\Illuminate\Http\Response|\Illuminate\Http\RedirectResponse)  $next
+     * @return \Illuminate\Http\Response|\Illuminate\Http\RedirectResponse
+     */
+    public function handle(Request $request, Closure $next)
+    {
+        Helper::BaseApiRequest()->filterRequest();
+        return $next($request);
+    }
 
 }
